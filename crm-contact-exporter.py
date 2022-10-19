0f5f6bf13804130f4a44b6680c675f3950f3ffe8
NikGorbunov <gorbunovmya.@gmail.com>
2022-10-19 18:19:06 +0300

    add env, fix edits

--- crm-contact-exporter.py
+++ crm-contact-exporter.py
@@ -4,79 +4,92 @@ import requests
 from hubspot import HubSpot
 import pandas as pd
 from simple_salesforce import Salesforce
-from sf_login import *
+
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
 
 today = date.today()
 
 
-def get_contacts_from_zoho():
-    url = "https://accounts.zoho.eu/oauth/v2/token"
-
+def get_zoho_access_token():
     payload = {'grant_type': 'authorization_code',
-               'client_id': '1000.GJW951HCMON3OZ49YLEO1JZC0HCUZY',
-               'client_secret': '0d36b23770308027169350713b92a02e3c6d9efcc1',
-               'redirect_uri': 'http://localhost:8000/test',
-               'code': '1000.cf449a8b14f0429ec8edac6b9cd3e690.ba0a5a149f2975ad4f27d7e51d404cde'}
-    files = []
-    headers = {}
+               'client_id': os.environ.get("ZOHO_CLIENT_ID"),
+               'client_secret': os.environ.get("ZOHO_CLIENT_SECRET"),
+               'redirect_uri': os.environ.get("ZOHO_REDIRECT_URI"),
+               'code': os.environ.get("ZOHO_CODE")}
 
-    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    response = requests.request("POST", os.environ.get("ZOHO_ACCESS_TOKEN_URL"), headers={}, data=payload, files=[])
+
     data = response.json()
+
     access_token = data['access_token']
 
-    url = "https://www.zohoapis.eu/crm/v2/Contacts"
+    return access_token
 
-    payload = {}
-    files = []
+
+def get_contacts_from_zoho():
+    access_token = get_zoho_access_token()
+
     headers = {
         'Authorization': f'Zoho-oauthtoken {access_token}'
     }
 
-    response = requests.request("GET", url, headers=headers, data=payload, files=files)
+    response = requests.request("GET", os.environ.get("ZOHO_CONTACTS_URL"), headers=headers, data={}, files=[])
 
     zoho_list = response.json()
-    data = zoho_list['data']
-    contacts_list = []
-    for contact in data:
-        contacts = {'email': contact['Email'], 'firstname': contact['First_Name'], 'lastname': contact['Last_Name'],
-                    'phone': contact['Phone'], 'title': contact['Title'], 'department': contact['Department']}
-        contacts_list.append(contacts)
-    contacts_zoho = pd.DataFrame(contacts_list).to_csv(f"reports/contacts_zoho-{today}.csv", index=False)
 
-    return contacts_zoho
+    data = zoho_list['data']
+
+    contacts_list = []
+
+    for contact in data:
+        contacts = {'Department': contact['Department'], 'First name': contact['First_Name'],
+                    'Last name': contact['Last_Name'], 'Email': contact['Email'],
+                    'Phone': contact['Phone'], 'Title': contact['Title']}
+        contacts_list.append(contacts)
+
+    zoho_contacts_df = pd.DataFrame(contacts_list)
+    zoho_contacts_df.to_csv(f"reports/zoho_contacts-{today}.csv", index=False)
+
+    return zoho_contacts_df
 
 
 def get_contacts_from_hubspot():
     api_client = HubSpot(
-        access_token="pat-eu1-8b6c2f2f-d75e-46b1-8a00-338ec3f66062")
-
-    api_client.access_token = "pat-eu1-8b6c2f2f-d75e-46b1-8a00-338ec3f66062"
+        access_token=os.environ.get("HUBSPOT_ACCESS_TOKEN"))
 
     hubspot_list = api_client.crm.contacts.get_all()
-    # print(hubspot_list)
 
     contacts_list = []
     for contact in hubspot_list:
-        contacts = {'email': contact.properties['email'], 'firstname': contact.properties['firstname'],
-                    'lastname': contact.properties['lastname']}
+        contacts = {'First name': contact.properties['firstname'], 'Last name': contact.properties['lastname'],
+                    'Email': contact.properties['email']
+                    }
         contacts_list.append(contacts)
-    contacts_hubspot = pd.DataFrame(contacts_list).to_csv(f"reports/contacts_hubspot-{today}.csv", index=False)
-    return contacts_hubspot
+
+    hubspot_contacts_df = pd.DataFrame(contacts_list)
+    hubspot_contacts_df.to_csv(f"reports/contacts_hubspot-{today}.csv", index=False)
+
+    return hubspot_contacts_df
 
 
 def get_contacts_from_salesforce():
-    sf = Salesforce(username=username,
-                    password=password,
-                    security_token=token,
-                    instance_url=instance)
+    sf = Salesforce(username=os.environ.get("SALESFORCE_USERNAME"),
+                    password=os.environ.get("SALESFORCE_PASSWORD"),
+                    security_token=os.environ.get("SALESFORCE_SECURITY_TOKEN"),
+                    instance_url=os.environ.get("SALESFORCE_INSTANCE_URL"))
 
-    contacts_data = sf.query_all("SELECT Email, Phone, Name, Title, Department FROM Contact")
-    contacts_sf = pd.DataFrame(contacts_data['records']).drop(columns='attributes').to_csv(f"reports/contacts_sf-{today}.csv",
-                                                                                           index=False)
-    return contacts_sf
+    contacts_data = sf.query_all("SELECT Department, Name, Email, Phone, Title FROM Contact")
+
+    salesforce_contacts_df = pd.DataFrame(contacts_data['records']).drop(columns='attributes')
+    salesforce_contacts_df.to_csv(f"reports/salesforce_contacts-{today}.csv", index=False)
+
+    return salesforce_contacts_df
 
 
 get_contacts_from_zoho()
 get_contacts_from_hubspot()
 get_contacts_from_salesforce()
-
